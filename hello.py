9443688133243ef725d2b41f05f522e9fca18f32
Lalibuck <a.buiko96@gmail.com>
2022-06-09 14:54:17 +0300

    3.4

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ def wsgi_app(env, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response(status, headers)
-    return ['\n'.join(env.get('QUERY-STRING').split('&'))]
+    body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+    return body
 
