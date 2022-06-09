f64d6aac06f39b0474d87864c54a4818866fcbda
Lalibuck <a.buiko96@gmail.com>
2022-06-09 14:52:05 +0300

    3.3

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ def wsgi_app(env, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response(status, headers)
-    return '\n'.join(env.get('QUERY-STRING').split('&'))
+    return ['\n'.join(env.get('QUERY-STRING').split('&'))]
 
