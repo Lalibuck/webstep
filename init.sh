733fa9c9d08fc11b9db249a2f20ec86c3324f619
Lalibuck <a.buiko96@gmail.com>
2022-06-13 17:20:13 +0300

    4.11

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
-sudo gunicorn -c /etc/gunicorn.d/django.py ask.wsgi:application
+sudo gunicorn -c /etc/gunicorn.d/django.py ask.ask.wsgi:application
