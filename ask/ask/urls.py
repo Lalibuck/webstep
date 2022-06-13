8360792e026aa027d4bdd3ce3da7cb5c3931cca3
Lalibuck <a.buiko96@gmail.com>
2022-06-13 12:21:36 +0300

    4.0

---
+++ ask/ask/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from ask.qa import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('/admin', admin.site.urls),
+    path('/', views.test),
+    path('login/', views.test),
+    path('signup/', views.test),
+    path('question/<int:id>/', views.test),
+    path('ask/', views.test),
+    path('popular/', views.test),
+    path('new/', views.test),
+]
