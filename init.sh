d53b871406d213b071d1253298b1e9bb344bc9dd
Lalibuck <a.buiko96@gmail.com>
2022-06-13 14:43:31 +0300

    4.2

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+source web/myvenv/bin/activate
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application &
